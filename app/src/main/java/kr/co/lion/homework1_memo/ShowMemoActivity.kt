f95139e38515b77ce238d91cab445f0e78754106
Jun <ayz1070@gmail.com>
2024-02-03 22:31:41 +0900

    ShowMemoActivity 제목, 날짜, 내용 데이터 화면에 표시

--- app/src/main/java/kr/co/lion/homework1_memo/ShowMemoActivity.kt
+++ app/src/main/java/kr/co/lion/homework1_memo/ShowMemoActivity.kt
@@ -1,11 +1,40 @@
 package kr.co.lion.homework1_memo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.lion.homework1_memo.databinding.ActivityShowMemoBinding
 
 class ShowMemoActivity : AppCompatActivity() {
+    lateinit var binding:ActivityShowMemoBinding
+    var memoData: Memo? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_memo)
+        binding = ActivityShowMemoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initData()
+        initView()
+        setEvent()
+    }
+
+    fun initData() {
+        memoData = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            intent?.getParcelableExtra("memoData",Memo::class.java)
+        }else{
+            intent?.getParcelableExtra<Memo>("memoData")
+        }
+    }
+
+    fun initView(){
+
+    }
+
+    fun setEvent(){
+        binding.apply {
+            editTextTitleShowMemo.setText(memoData?.title)
+            editTextContentShowMemo.setText(memoData?.content)
+            editTextDateShowMemo.setText(memoData?.date)
+        }
     }
 }
