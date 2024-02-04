9c44d49ad19dda6caa4dc28d2c3a633ec0b19eb4
Jun <ayz1070@gmail.com>
2024-02-04 10:51:02 +0900

    ModifyMemoActivity 수정 구현

--- app/src/main/java/kr/co/lion/homework1_memo/ModifyMemoActivity.kt
+++ app/src/main/java/kr/co/lion/homework1_memo/ModifyMemoActivity.kt
@@ -1,11 +1,104 @@
 package kr.co.lion.homework1_memo
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import kr.co.lion.homework1_memo.databinding.ActivityModifyMemoBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class ModifyMemoActivity : AppCompatActivity() {
+    lateinit var binding:ActivityModifyMemoBinding
+    lateinit var memoTitle:String
+    lateinit var memoContent:String
+
+    var memoData:Memo? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_modify_memo)
+        binding = ActivityModifyMemoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initData()
+        initView()
+        setEvent()
+
+    }
+
+    fun setToolbar(){
+        binding.apply{
+            toolbarModifyMemo.apply{
+                title = "메모 수정"
+                inflateMenu(R.menu.menu_modify_memo)
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    finish()
+                }
+            }
+        }
+    }
+
+    fun initData(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            memoData = intent?.getParcelableExtra("memoData",Memo::class.java)
+        }else{
+            memoData = intent?.getParcelableExtra<Memo>("memoData")
+        }
+
+    }
+
+    fun initView(){
+        setToolbar()
+        binding.apply{
+            editTextTitleModifyMemo.setText(memoData?.title)
+            editTextContentModifyMemo.setText(memoData?.content)
+        }
+    }
+
+    fun setEvent(){
+        binding.apply{
+            toolbarModifyMemo.apply{
+                setOnMenuItemClickListener{
+                    when(it.itemId){
+                        R.id.menu_item_complete_modify_memo -> {
+                            processInput()
+                        }
+                    }
+                    true
+                }
+
+            }
+        }
+    }
+
+    fun processInput() {
+        binding.apply {
+            // 예외 처리 필요
+            memoTitle = editTextTitleModifyMemo.text.toString()
+            if (memoTitle.isEmpty()) {
+                Snackbar.make(binding.root, "빈 칸이 존재합니다!!", Snackbar.LENGTH_SHORT).show()
+                return
+            }
+            memoContent = editTextContentModifyMemo.text.toString()
+            if (memoContent.isEmpty()) {
+                Snackbar.make(binding.root, "빈 칸이 존재합니다!!", Snackbar.LENGTH_SHORT).show()
+                return
+            }
+
+            val modifiedMemoData = Memo(memoTitle, memoContent,getCurrentDate())
+            val resultIntent = Intent()
+            resultIntent.putExtra("modifiedMemo",modifiedMemoData)
+
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+
+    }
+    fun getCurrentDate():String{
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val currentDate = Date()
+        return dateFormat.format(currentDate)
     }
 }
--- app/src/main/java/kr/co/lion/homework1_memo/ShowMemoActivity.kt
+++ app/src/main/java/kr/co/lion/homework1_memo/ShowMemoActivity.kt
@@ -1,13 +1,21 @@
 package kr.co.lion.homework1_memo
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import kr.co.lion.homework1_memo.databinding.ActivityShowMemoBinding
 
 class ShowMemoActivity : AppCompatActivity() {
     lateinit var binding:ActivityShowMemoBinding
+    lateinit var modifyMemoLauncher:ActivityResultLauncher<Intent>
+
     var memoData: Memo? = null
+    var modifiedMemo: Memo? =null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShowMemoBinding.inflate(layoutInflater)
@@ -18,16 +26,63 @@ class ShowMemoActivity : AppCompatActivity() {
         setEvent()
     }
 
+    fun setToolbar(){
+        binding.apply{
+            toolbarShowMemo.apply{
+                title = "메모 보기"
+                inflateMenu(R.menu.menu_show_memo)
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    finish()
+                }
+                setOnMenuItemClickListener {
+                    when(it.itemId){
+                        R.id.menu_item_modify_show_memo -> {
+                            val newIntent = Intent(this@ShowMemoActivity,ModifyMemoActivity::class.java)
+                            newIntent.putExtra("memoData",memoData)
+                            modifyMemoLauncher.launch(newIntent)
+                        }
+
+                        R.id.menu_item_delete_show_memo -> {
+
+                        }
+                    }
+                    true
+                }
+            }
+        }
+    }
+
     fun initData() {
         memoData = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
             intent?.getParcelableExtra("memoData",Memo::class.java)
         }else{
             intent?.getParcelableExtra<Memo>("memoData")
         }
+
+        val contractModifyMemo = ActivityResultContracts.StartActivityForResult()
+        modifyMemoLauncher = registerForActivityResult(contractModifyMemo){
+            if(it.resultCode == RESULT_OK){
+                // 구현
+                if(it.resultCode == RESULT_OK){
+                    if(it.data != null){
+                        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
+                            modifiedMemo = it.data?.getParcelableExtra("modifiedMemo",Memo::class.java)
+                        }else{
+                            modifiedMemo = it.data?.getParcelableExtra<Memo>("modifiedMemo")
+                        }
+                        binding.editTextTitleShowMemo.setText(modifiedMemo?.title)
+                        binding.editTextContentShowMemo.setText(modifiedMemo?.content)
+                        binding.editTextDateShowMemo.setText("${modifiedMemo?.date} (수정됨)")
+                    }
+                }
+            }
+        }
+
     }
 
     fun initView(){
-
+        setToolbar()
     }
 
     fun setEvent(){
